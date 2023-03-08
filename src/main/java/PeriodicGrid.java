46d9ae4452844f70435d99f26a7eeed442669a1d
shadad00 <shadad00@itba.edu.ar>
2023-03-08 11:02:25 -0300

    Changing createVirtualParticle method

--- src/main/java/PeriodicGrid.java
+++ src/main/java/PeriodicGrid.java
@@ -7,8 +7,7 @@ public class PeriodicGrid extends NoPeriodicGrid{
 
     @Override
     protected void initializeCell(int dim){
-        dim = dim + 2 ;
-        super.initializeCell(dim);
+        super.initializeCell(dim + 2); //adding extra rows and columns
     }
 
     @Override
@@ -21,38 +20,40 @@ public class PeriodicGrid extends NoPeriodicGrid{
 
 
     private void createVirtualParticle(int row , int col, Particle particle){
+        int newRow=-1, newCol=-1;
         Particle diagonalVirtualParticle = particle.getVirtualParticle();
-        Particle columnVirtualParticle = particle.getVirtualParticle();
-        Particle rowVirtualParticle = particle.getVirtualParticle();
-        int newRow = row;
-        int newCol = col;
 
-        if(row == 1){ //from first to last row
-            rowVirtualParticle.setY(rowVirtualParticle.getY() - this.length);
-            newRow = this.cellQuantity + 1 ;
+        if(row == 1 || row == this.cellQuantity){
+            Particle rowVirtualParticle = particle.getVirtualParticle();
+            if(row == 1){
+                rowVirtualParticle.setY(rowVirtualParticle.getY() - this.length);
+                newRow = this.cellQuantity + 1 ;
+            }else{
+                rowVirtualParticle.setY(rowVirtualParticle.getY() + this.length);
+                newRow = 0 ;
+            }
             cells[newRow][col].addParticle(rowVirtualParticle);
             diagonalVirtualParticle.setY(rowVirtualParticle.getY());
         }
-        else if(row == this.cellQuantity) {//from last to first row
-            rowVirtualParticle.setY(rowVirtualParticle.getY() + this.length);
-            newRow = 0 ;
-            cells[newRow][col].addParticle(rowVirtualParticle);
-            diagonalVirtualParticle.setY(rowVirtualParticle.getY());
-        }
-        if( col == 1 ){
-            columnVirtualParticle.setX(columnVirtualParticle.getX() + this.length);
-            newCol = this.cellQuantity + 1 ;
+        if(col == 1 || col == this.cellQuantity){
+            Particle columnVirtualParticle = particle.getVirtualParticle();
+            if( col == 1 ){
+                columnVirtualParticle.setX(columnVirtualParticle.getX() + this.length);
+                newCol = this.cellQuantity + 1 ;
+            }
+            else{
+                columnVirtualParticle.setX(columnVirtualParticle.getX() - this.length);
+                newCol = 0;
+            }
             cells[row][newCol].addParticle(columnVirtualParticle);
             diagonalVirtualParticle.setX(columnVirtualParticle.getX());
         }
-        else if (col == this.cellQuantity ){
-            columnVirtualParticle.setX(columnVirtualParticle.getX() - this.length);
-            newCol = 0;
-            cells[row][newCol].addParticle(columnVirtualParticle);
-            diagonalVirtualParticle.setX(columnVirtualParticle.getX());
-        }
-        if (!(particle.getX() == diagonalVirtualParticle.getX() && particle.getY() == diagonalVirtualParticle.getY()))
+
+        if (newRow >= 0 && newCol >= 0)
             cells[newRow][newCol].addParticle(diagonalVirtualParticle);
     }
+
+
+
 
 }
