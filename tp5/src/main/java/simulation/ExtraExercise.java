19f22891833903d0cdc8a45c37b36bc45ebffc86
Joaquin Suarez Bodean <joaco@after-math.es>
2023-06-06 11:58:25 -0300

    feat: Add no oscilation silo

---
+++ tp5/src/main/java/simulation/ExtraExercise.java
@@ -0,0 +1,38 @@
+package simulation;
+
+import java.io.IOException;
+
+public class ExtraExercise {
+
+    private static final int OPTIMAL_FREQUENCY = 0;
+    private static final double[] GAPS = {0.04, 0.05, 0.06};
+
+    private static final int SIMULATION_PER_FREQUENCY = 1;
+
+    public static void main(String[] args) {
+        runAll();
+    }
+
+    public static void runAll()  {
+        for (double gap : GAPS) {
+            for (int i = 0; i < SIMULATION_PER_FREQUENCY; i++) {
+                Table currentTable = new Table(gap, OPTIMAL_FREQUENCY);
+                run(currentTable , 10,gap, i );
+            }
+        }
+    }
+
+    public static void run(Table currentTable, int persistingMultiplier,double gap,  int j) {
+        String outputFilename = String.format("no_oscilation_silo_gap%.2f_i%d",gap,  j );
+        try {
+            System.out.println("Generating " + outputFilename + "...");
+            new CsvGenerator("tp5/out/",
+                    outputFilename,
+                    currentTable,
+                    persistingMultiplier
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
