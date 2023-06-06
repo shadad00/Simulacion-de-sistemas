50f598f02742261599665fcddeb1cbd1ee76c14a
Joaquin Suarez Bodean <joaco@after-math.es>
2023-06-06 17:13:50 -0300

    feat: Include multiseries plot for different gaps

--- tp5/src/main/java/simulation/ExtraExercise.java
+++ tp5/src/main/java/simulation/ExtraExercise.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class ExtraExercise {
 
-    private static final int OPTIMAL_FREQUENCY = 0;
+    private static final int FREQUENCY = 0;
     private static final double[] GAPS = {0.04, 0.05, 0.06};
 
     private static final int SIMULATION_PER_FREQUENCY = 1;
@@ -16,14 +16,14 @@ public class ExtraExercise {
     public static void runAll()  {
         for (double gap : GAPS) {
             for (int i = 0; i < SIMULATION_PER_FREQUENCY; i++) {
-                Table currentTable = new Table(gap, OPTIMAL_FREQUENCY);
-                run(currentTable , 10,gap, i );
+                Table currentTable = new Table(gap, FREQUENCY);
+                run(currentTable,10, gap, i);
             }
         }
     }
 
     public static void run(Table currentTable, int persistingMultiplier,double gap,  int j) {
-        String outputFilename = String.format("no_oscilation_silo_gap%.2f_i%d",gap,  j );
+        String outputFilename = String.format("silo_fq0_gap%.2f_i%d", gap, j);
         try {
             System.out.println("Generating " + outputFilename + "...");
             new CsvGenerator("tp5/out/",
--- tp5/src/main/java/simulation/SecondExercise.java
+++ tp5/src/main/java/simulation/SecondExercise.java
@@ -5,8 +5,7 @@ import java.io.IOException;
 public class SecondExercise {
 
     private static final int OPTIMAL_FREQUENCY = 10;
-    private static final double[] GAPS = {0.04,0.05,0.06};
-
+    private static final double[] GAPS = {0.04, 0.05, 0.06};
 
     private static final int SIMULATION_PER_FREQUENCY = 1;
 
@@ -25,7 +24,7 @@ public class SecondExercise {
     }
 
     public static void run(Table currentTable, int persistingMultiplier,double gap,  int j) {
-        String outputFilename = String.format("silo_gap%.2f_i%d",gap,  j );
+        String outputFilename = String.format("silo_fq%d_gap%.2f_i%d", OPTIMAL_FREQUENCY, gap, j);
         try {
             System.out.println("Generating " + outputFilename + "...");
             new CsvGenerator("tp5/out/",
